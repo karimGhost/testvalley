ddfc173affb913547a50d32c7581499b4f3ec772
Karim <abdulkarimkassimsalim@gmail.com>
2024-02-10 12:45:43 +0300

    Your commit message

---
+++ next.config.js
@@ -0,0 +1,12 @@
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+    reactStrictMode: true,
+    basePath: '/testvalley',
+    output: 'export'
+
+
+  }
+  module.exports = nextConfig
